a6ef9e02170f435a015f2ea57eca3626d1937770
linczyc-MLX <linczyc@gmail.com>
2026-01-01 11:02:53 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './styles/index.css';
 import './styles/design-identity-styles.css';
 import './styles/taste-exploration.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
